aac4c2d401a9d2e3f156573d5d98662f09426f6b
Nico Bos <nico@bos.codes>
2025-06-13 14:41:59 +0200

    soma wip

--- src/cards/sum.rs
+++ src/cards/sum.rs
@@ -1,6 +1,30 @@
-use leptos::prelude::*;
+use leptos::{logging::log, prelude::*};
 
 use crate::locales::i18n::I18n;
+
+// create calulation error type
+#[derive(Debug)]
+pub struct CalculationError {
+    pub message: String,
+}
+
+pub fn calculate_sum(num1: i32, num2: i32, operator: &str) -> Result<i32, CalculationError> {
+    match operator {
+        "+" => Ok(num1 + num2),
+        "-" => Ok(num1 - num2),
+        "x" | "X" | "*" => Ok(num1 * num2),
+        "/" => {
+            if num2 != 0 {
+                Ok(num1 / num2)
+            } else {
+                Ok(0)
+            }
+        } // Avoid division by zero
+        _ => Err(CalculationError {
+            message: format!("{} is not a supported operator", operator),
+        }),
+    }
+}
 
 /// Renders the home page of your application.
 #[allow(non_snake_case)]
@@ -11,10 +35,18 @@ pub fn SumCard() -> impl IntoView {
     let sum_result = RwSignal::new(0);
     let num1 = RwSignal::new("0".to_string());
     let num2 = RwSignal::new("0".to_string());
+    let operator = RwSignal::new("+".to_string());
 
     let calculate_sum = move |_| {
-        let sum = num1.get().parse::<i32>().unwrap_or(0) + num2.get().parse::<i32>().unwrap_or(0);
-        sum_result.set(sum);
+        let sum = calculate_sum(
+            num1.get().parse::<i32>().unwrap_or(0),
+            num2.get().parse::<i32>().unwrap_or(0),
+            operator.get().as_str(),
+        );
+        sum_result.set(sum.unwrap_or_else(|e| {
+            leptos::logging::error!("Calculation error: {}", e.message);
+            0 // Default to 0 on error
+        }));
     };
 
     view! {
@@ -22,7 +54,7 @@ pub fn SumCard() -> impl IntoView {
         <a class="card__title">{move || i18n.get().t("sum_calculator").to_string()}</a>
         <div class="card__inputs">
           <input type="text" pattern="[0-9]*" bind:value=num1 />
-          <p>+</p>
+          <input type="text" pattern="[0-9]*" bind:value=operator />
           <input type="text" pattern="[0-9]*" bind:value=num2 />
           <button class="card__inputs__button" on:click=calculate_sum>{move || i18n.get().t("calculate_sum").to_string()}</button>
         </div>
